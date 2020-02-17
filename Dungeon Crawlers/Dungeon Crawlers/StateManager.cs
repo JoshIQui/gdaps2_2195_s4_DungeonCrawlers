6deb5650c6f5591fefa09fc7bdcf81d721a5a9de
Joshua Quinones <jiq4449@ad.rit.edu>
2020-02-17 18:53:18 -0500

    Created outline for State manager class

---
+++ Dungeon Crawlers/Dungeon Crawlers/StateManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawlers
+{
+    //The different parts of the game's state
+    enum GameState
+    {
+        Title,
+        Instructions,
+        Game,
+        GameOver,
+        Win
+    }
+    class StateManager
+    {
+        // ---------------------
+        // Fields
+        // ---------------------
+
+        private GameState currentState;
+
+        // ---------------------
+        // Properties
+        // ---------------------
+
+        public GameState CurrentState
+        {
+            get { return currentState; }
+            set { currentState = value; }
+        }
+
+        // ---------------------
+        // Constructor
+        // ---------------------
+        public StateManager()
+        {
+            this.currentState = GameState.Title;
+        }
+
+        // ---------------------
+        // Methods
+        // ---------------------
+
+        //Changes the state of the game to the parameter
+        public void ChangeState(GameState newState)
+        {
+            switch (newState)
+            {
+                case GameState.Title:
+                    currentState = GameState.Title;
+                    break;
+
+                case GameState.Instructions:
+                    currentState = GameState.Instructions;
+                    break;
+
+                case GameState.Game:
+                    currentState = GameState.Game;
+                    break;
+
+                case GameState.GameOver:
+                    currentState = GameState.GameOver;
+                    break;
+
+                case GameState.Win:
+                    currentState = GameState.Win;
+                    break;
+            }
+        }
+
+        //ADD LOGIC HERE
+        public void DecideState()
+        {
+            GameState decidedState = GameState.Title;
+
+            this.ChangeState(decidedState);
+        }
+    }
+}
+ 
