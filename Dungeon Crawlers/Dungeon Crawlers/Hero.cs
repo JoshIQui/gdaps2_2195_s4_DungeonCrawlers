5415574acf6587ac23a4b42bdba34e174a5e442f
kri2889 <kri2889@IGM2025-29>
2020-02-22 13:31:20 -0500

    Added Hero contructor and fields

--- Dungeon Crawlers/Dungeon Crawlers/Hero.cs
+++ Dungeon Crawlers/Dungeon Crawlers/Hero.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 namespace Dungeon_Crawlers
 {
     class Hero : GameObject, IHaveAI
     {
+        // Fields
+        Texture2D asset;
+        Rectangle position;
+        private int health;
+        private int width;
+        private int height;
+        public int Health
+        {
+            get { return health; }
+        }
+
+        public Hero(Texture2D asset, Rectangle position, GraphicsDeviceManager graphics, int screenWidth, int screenHeight, int health = 100)
+            :base (asset,position,graphics)
+        {
+            this.health = health;
+            this.width = screenWidth;
+            this.health = screenHeight;
+        }
         protected override bool CheckCollision(List<Hitbox> objects)
         {
+
             return false;
         }
     }
