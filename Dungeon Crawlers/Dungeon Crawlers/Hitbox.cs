d2b267e0c4839b6a0a80b3b83f31f98b9f51655c
Joshua Quinones <jiq4449@g.rit.edu>
2020-02-22 13:30:30 -0500

    Worked on the hitbox class

--- Dungeon Crawlers/Dungeon Crawlers/Hitbox.cs
+++ Dungeon Crawlers/Dungeon Crawlers/Hitbox.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
 
 namespace Dungeon_Crawlers
 {
+    enum BoxType
+    {
+        Hitbox,
+        Hurtbox,
+        Collision
+    }
+
     class Hitbox
     {
+        // ---------------------
+        // Fields
+        // ---------------------
+
+        private Rectangle box;
+        private BoxType boxType;
+        private bool hasDuration;
+
+        // ---------------------
+        // Properties
+        // ---------------------
+
+        public Rectangle Box
+        {
+            get { return this.box; }
+            set { this.box = value; }
+        }
+
+        public BoxType BoxType
+        {
+            get { return this.boxType; }
+        }
+
+        // ---------------------
+        // Constructor
+        // ---------------------
+
+        public Hitbox(Rectangle box, BoxType boxType)
+        {
+            this.box = box;
+            this.boxType = boxType;
+        }
     }
 }
