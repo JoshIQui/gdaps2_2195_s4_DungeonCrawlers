1067924884b1222f493ae2676b13a361a130229a
rdr8959 <rdr8959@IGM2025-28>
2020-02-22 13:33:32 -0500

    Updated GameObject and Player Class to implement hitboxes

--- Dungeon Crawlers/Dungeon Crawlers/Player.cs
+++ Dungeon Crawlers/Dungeon Crawlers/Player.cs
@@ -12,30 +12,48 @@ namespace Dungeon_Crawlers
     class Player : GameObject
     {
         // Fields
-        Texture2D asset;
-        Rectangle position;
-        int health;
-        int numEnemies;
+        private int health;
+        private int numEnemies;
 
         // Properties
         public int Health
         {
             get { return health; }
+            set { health = value; }
+        }
+
+        public int NumEnemies
+        {
+            get { return numEnemies; }
+            set { numEnemies = value; }
         }
 
         // Constructor
-        public Player(Texture2D asset, Rectangle position, GraphicsDeviceManager graphics, int screenWidth, int screenHeight, int health = 100, int numEnemies = 0)
+        public Player(Texture2D asset, Hitbox position, int screenWidth, int screenHeight, int health = 100, int numEnemies = 0)
+            : base(asset, position, graphics)
         {
             this.asset = asset;
             this.position = position;
             this.graphics = graphics;
+            this.health = health;
+            this.numEnemies = numEnemies;
         }
 
         // Methods
-        public override void Update()
+
+        // Method for Player Updates in game
+        public override void Update(GameTime gametime)
         {
 
         }
+
+        // Method for Drawing the Player
+        public override void Draw(SpriteBatch sb)
+        {
+            
+        }
+
+        // Method for Collision Checks for player
         protected override bool CheckCollision(List<Hitbox> objects)
         {
             return false;
