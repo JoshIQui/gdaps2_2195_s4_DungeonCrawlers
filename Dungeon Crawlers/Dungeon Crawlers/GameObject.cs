2bf329dcde26ec0a8bea56ea1762efb84620a556
rdr8959 <rdr8959@IGM2025-28>
2020-02-22 13:16:24 -0500

    Filling in OO structure for Player and GameObject

--- Dungeon Crawlers/Dungeon Crawlers/GameObject.cs
+++ Dungeon Crawlers/Dungeon Crawlers/GameObject.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Dungeon_Crawlers
 {
@@ -10,13 +13,27 @@ namespace Dungeon_Crawlers
     {
         // Fields
         protected bool isDamageable;
+        protected Texture2D asset;
+        protected Rectangle position;
+        protected GraphicsDeviceManager graphics;
 
         // Properties
 
         // Constructor
+        public GameObject(Texture2D asset, Rectangle position, GraphicsDeviceManager graphics)
+        {
 
+        }
         // Methods
         abstract protected bool CheckCollision(List<Hitbox> objects);
+
+        // Method that draws the asset
+        public virtual void Draw(SpriteBatch sb)
+        {
+            sb.Draw(asset, position, Color.White);
+        }
+        // Method that updates object
+        public abstract void Update(GameTime gameTime);
 
     }
 }
--- Dungeon Crawlers/Dungeon Crawlers/Player.cs
+++ Dungeon Crawlers/Dungeon Crawlers/Player.cs
@@ -3,25 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Dungeon_Crawlers
 {
     class Player : GameObject
     {
         // Fields
+        Texture2D asset;
+        Rectangle position;
+        int health;
+        int numEnemies;
 
         // Properties
+        public int Health
+        {
+            get { return health; }
+        }
 
         // Constructor
-        public Player(int screenWidth, int screenHeight, int levelWidth, int levelHeight)
+        public Player(Texture2D asset, Rectangle position, GraphicsDeviceManager graphics, int screenWidth, int screenHeight, int health = 100, int numEnemies = 0)
         {
-
+            this.asset = asset;
+            this.position = position;
+            this.graphics = graphics;
         }
 
         // Methods
+        public override void Update()
+        {
+
+        }
         protected override bool CheckCollision(List<Hitbox> objects)
         {
             return false;
         }
+
+
     }
 }
