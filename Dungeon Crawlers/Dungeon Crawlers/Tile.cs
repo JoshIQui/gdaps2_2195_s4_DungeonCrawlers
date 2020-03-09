00a3dad666f6ee57500775e72ca47f38f686b8c6
mythguy1226 <robbiereddick@aol.com>
2020-03-08 20:43:19 -0400

    Updated StateManager to be Singleton, Updated Tile class to differentiate with enums, and Updated Player to take damage and go to GameOver screen once at 0

--- Dungeon Crawlers/Dungeon Crawlers/Tile.cs
+++ Dungeon Crawlers/Dungeon Crawlers/Tile.cs
@@ -9,12 +9,43 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Dungeon_Crawlers
 {
+    enum TileType
+    {
+        Floor,
+        Spikes,
+        LeftStairs,
+        RightStairs,
+        LeftLedge,
+        RightLedge,
+        Platform,
+        VerticalDivider,
+        HorizontalDivider,
+        LeftHalfTile,
+        RightHalfTile,
+        TopHalfTile,
+        BottomHalfTile,
+        TopLeftSlantCorner,
+        TopRightSlantCorner,
+        BottomRightSlantCorner,
+        BottomLeftSlantCorner,
+        TopLeftFullCorner,
+        TopRightFullCorner,
+        BottomRightFullCorner,
+        BottomLeftFullCorner,
+        TopLeftTriangle,
+        TopRightTriangle,
+        BottomRightTriangle,
+        BottomLeftTriangle,
+        BlackBlock
+
+    }
     class Tile
     {
         // Fields
         protected bool isDamageable;
         protected Texture2D asset;
         protected Hitbox position;
+        protected TileType type;
 
         private int width = 64;
         private int height = 64;
@@ -32,16 +63,25 @@ namespace Dungeon_Crawlers
         }
 
         // Constructor
-        public Tile(Texture2D asset, Hitbox position)
+        public Tile(Texture2D asset, Hitbox position, TileType type)
         {
             this.asset = asset;
             this.position = position;
+            this.type = type;
         }
 
         // Method that draws the asset
         public void Draw(SpriteBatch sb)
         {
-            DrawFloor(SpriteEffects.None, sb);
+            switch (type)
+            {
+                case TileType.Spikes:
+                    DrawSpikes(SpriteEffects.None, sb);
+                    break;
+                case TileType.Floor:
+                    DrawFloor(SpriteEffects.None, sb);
+                    break;
+            }
         }
 
         private void DrawFloor(SpriteEffects flipSprite, SpriteBatch spriteBatch)
@@ -52,6 +92,24 @@ namespace Dungeon_Crawlers
                 new Rectangle(                  // - The "source" rectangle
                     0 * width,     //   - This rectangle specifies
                     offset * 0,           //	   where "inside" the texture
+                    width,             //     to get pixels (We don't want to
+                    height),           //     draw the whole thing)
+                Color.White,                    // - The color
+                0,                              // - Rotation (none currently)
+                Vector2.Zero,                   // - Origin inside the image (top left)
+                2.0f,                           // - Scale (100% - no change)
+                flipSprite,                     // - Can be used to flip the image
+                0);                             // - Layer depth (unused)
+        }
+
+        private void DrawSpikes(SpriteEffects flipSprite, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                asset,                    // - The texture to draw
+                new Vector2(position.BoxX, position.BoxY),                       // - The location to draw on the screen
+                new Rectangle(                  // - The "source" rectangle
+                    5 * width,     //   - This rectangle specifies
+                    offset * 1,           //	   where "inside" the texture
                     width,             //     to get pixels (We don't want to
                     height),           //     draw the whole thing)
                 Color.White,                    // - The color
